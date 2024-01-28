b5b40506717c517c0127bc08098fcecff1abaf1c
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2024-01-28 13:18:47 +0000

    feat(): click sentence to skip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,78 @@
-"use client";
+"use client"
 
-import { Button } from "@/components/button";
-import { useEffect, useRef, useState } from "react";
-import { PauseCircleIcon, PlayCircleIcon } from "@heroicons/react/16/solid";
-import { chunks } from "./chunks-pg";
+import { Button } from "@/components/button"
+import { useEffect, useRef, useState } from "react"
+import { PauseCircleIcon, PlayCircleIcon } from "@heroicons/react/16/solid"
+import { chunks } from "./chunks-pg"
 // import { playAudio } from "./play"
-import Record from "@/components/record";
+import Record from "@/components/record"
 
 import {
   Alert,
   AlertActions,
   AlertDescription,
   AlertTitle,
-} from "@/components/alert";
+} from "@/components/alert"
+import clsx from "clsx"
 
 export default function Page() {
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-  const [reading, setReading] = useState(false);
-  const [cursorIndex, setCursorIndex] = useState(0);
-  const [contextString, setContextString] = useState("");
-  const [speed, setSpeed] = useState(1);
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+  const [reading, setReading] = useState(false)
+  const [cursorIndex, setCursorIndex] = useState(0)
+  const [contextString, setContextString] = useState("")
+  const [speed, setSpeed] = useState(1)
   let [alertState, setAlertState] = useState<{
-    open: boolean;
-    question: string | null;
-    response: string | null;
+    open: boolean
+    question: string | null
+    response: string | null
   }>({
     open: false,
     question: null,
     response: null,
-  });
+  })
 
   useEffect(() => {
-    console.log("playing", cursorIndex);
-    var audio = new Audio(`/audio/pg/audio-${cursorIndex}.mp3`);
-    audioRef.current = audio;
-    audio.playbackRate = speed;
+    console.log("playing", cursorIndex)
+    var audio = new Audio(`/audio/pg/audio-${cursorIndex}.mp3`)
+    audioRef.current = audio
+    audio.playbackRate = speed
     if (cursorIndex > 0) {
       audio.oncanplaythrough = () => {
-        console.log("canplaythrough", cursorIndex);
-        audio.play();
-      };
+        console.log("canplaythrough", cursorIndex)
+        audio.play()
+      }
     }
 
     audio.onended = () => {
-      console.log("ended", cursorIndex);
-      setCursorIndex((x) => x + 1);
-    };
+      console.log("ended", cursorIndex)
+      setCursorIndex((x) => x + 1)
+    }
     return () => {
-      audio.pause();
-    };
-  }, [cursorIndex]);
+      audio.pause()
+    }
+  }, [cursorIndex])
 
   useEffect(() => {
-    audioRef.current?.pause();
+    audioRef.current?.pause()
     if (audioRef.current?.playbackRate) {
-      audioRef.current.playbackRate = speed;
+      audioRef.current.playbackRate = speed
     }
-    audioRef.current?.play();
-  }, [speed]);
+    audioRef.current?.play()
+  }, [speed])
 
   function play() {
-    audioRef?.current?.play();
-    setReading(true);
+    audioRef?.current?.play()
+    setReading(true)
   }
 
   function pause() {
-    audioRef?.current?.pause();
-    setReading(false);
+    audioRef?.current?.pause()
+    setReading(false)
   }
 
   useEffect(() => {
-    setContextString(chunks.slice(0, cursorIndex + 1).join("\n"));
-  }, [cursorIndex]);
+    setContextString(chunks.slice(0, cursorIndex + 1).join("\n"))
+  }, [cursorIndex])
 
   // useEffect(() => {
   //   if (reading) {
@@ -95,36 +96,46 @@ export default function Page() {
           <Button onClick={() => setSpeed(2)}>2x</Button>
         </div>
         <hr />
-        {chunks.map((x, id) => (
-          <p key={id}>{x}</p>
+        {chunks.map((x, idx) => (
+          <p
+            key={idx}
+            className={clsx(
+              cursorIndex === idx ? "bg-yellow-100 dark:bg-yellow-900" : ""
+            )}
+            onClick={() => {
+              setCursorIndex(idx)
+            }}
+          >
+            {x}
+          </p>
         ))}
       </article>
       <div className="fixed bottom-4 right-4 space-y-2">
         <Record
           contextString={contextString}
           onClick={(recording) => {
-            pause();
+            pause()
             if (!recording) {
-              return;
+              return
             }
-            var audio = new Audio(`/audio/ding.mp3`);
-            audio.playbackRate = 1;
+            var audio = new Audio(`/audio/ding.mp3`)
+            audio.playbackRate = 1
             audio.oncanplaythrough = () => {
-              audio.play();
-            };
+              audio.play()
+            }
           }}
           onResponse={({
             question,
             response,
           }: {
-            question: string | null;
-            response: string | null;
+            question: string | null
+            response: string | null
           }) => {
             setAlertState({
               open: true,
               question,
               response,
-            });
+            })
           }}
         />
         <Button>
@@ -144,15 +155,23 @@ export default function Page() {
         <AlertDescription>{alertState.response}</AlertDescription>
         <AlertActions>
           <Button
+            plain
             onClick={() => {
-              setAlertState((prev) => ({ ...prev, open: false }));
-              play(); // Call the play function to start playing the audio
+              setAlertState((prev) => ({ ...prev, open: false }))
             }}
           >
             Close
           </Button>
+          <Button
+            onClick={() => {
+              setAlertState((prev) => ({ ...prev, open: false }))
+              play()
+            }}
+          >
+            Continue
+          </Button>
         </AlertActions>
       </Alert>
     </div>
-  );
+  )
 }
