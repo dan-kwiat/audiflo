6f4252487db50e6a90fa02ef712caeda37e75d95
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2024-01-28 10:40:28 +0000

    feat(data): persist audio and read from file

---
+++ scripts/download-audio.ts
@@ -0,0 +1,84 @@
+require("dotenv").config({ path: ".env.local" })
+import fs from "fs"
+import { chunks } from "../src/app/chunks-pg"
+// interface VoiceSettings {
+//   stability: number
+//   similarity_boost: number
+// }
+
+// interface TextToSpeechData {
+//   text: string
+//   model_id: string
+//   voice_settings: VoiceSettings
+// }
+
+const CHUNK_SIZE = 1024
+const voiceId = "eXveFzitr3hBwS16SAe9"
+const url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`
+const headers = {
+  Accept: "audio/mpeg",
+  "Content-Type": "application/json",
+  "xi-api-key": process.env.NEXT_PUBLIC_ELEVENLABS_API_KEY || "",
+}
+
+async function persistAudio(id: string, text: string) {
+  const data = {
+    text,
+    model_id: "eleven_multilingual_v2",
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.5,
+    },
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data),
+    })
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok.")
+    }
+
+    // write audio response to file with fs
+    const reader = response.body.getReader()
+    const writer = fs.createWriteStream(`./data/pg/audio-${id}.mp3`)
+
+    let receivedLength = 0
+    let chunks = []
+
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) {
+        break
+      }
+
+      chunks.push(value)
+      receivedLength += value.length
+    }
+
+    const chunksAll = new Uint8Array(receivedLength)
+    let position = 0
+    for (let chunk of chunks) {
+      chunksAll.set(chunk, position)
+      position += chunk.length
+    }
+
+    writer.write(chunksAll)
+  } catch (error) {
+    console.error("Error:", error)
+  }
+}
+
+async function main() {
+  for (let i = 0; i < chunks.length; i++) {
+    const chunk = chunks[i]
+    await persistAudio(i.toString(), chunk)
+  }
+}
+
+main()
+
+export {}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,97 @@
 "use client"
 
 import { Button } from "@/components/button"
-import { use, useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { PauseCircleIcon, PlayCircleIcon } from "@heroicons/react/16/solid"
-import { chunks } from "./chunks"
-import { playAudio } from "./play"
+import { chunks } from "./chunks-pg"
+// import { playAudio } from "./play"
 import Record from "@/components/record"
 
 export default function Page() {
+  const audioRef = useRef<HTMLAudioElement | null>(null)
   const [reading, setReading] = useState(false)
   const [cursorIndex, setCursorIndex] = useState(0)
   const [contextString, setContextString] = useState("")
+  const [speed, setSpeed] = useState(1)
+
+  useEffect(() => {
+    console.log("playing", cursorIndex)
+    var audio = new Audio(`/audio/pg/audio-${cursorIndex}.mp3`)
+    audioRef.current = audio
+    audio.playbackRate = speed
+    if (cursorIndex > 0) {
+      audio.oncanplaythrough = () => {
+        console.log("canplaythrough", cursorIndex)
+        audio.play()
+      }
+    }
+
+    audio.onended = () => {
+      console.log("ended", cursorIndex)
+      setCursorIndex((x) => x + 1)
+    }
+    return () => {
+      audio.pause()
+    }
+  }, [cursorIndex])
+
+  useEffect(() => {
+    audioRef.current?.pause()
+    if (audioRef.current?.playbackRate) {
+      audioRef.current.playbackRate = speed
+    }
+    audioRef.current?.play()
+  }, [speed])
+
+  function togglePlay() {
+    if (reading) {
+      audioRef?.current?.pause()
+    } else {
+      audioRef?.current?.play()
+    }
+    setReading((x) => !x)
+  }
 
   useEffect(() => {
     setContextString(chunks.slice(0, cursorIndex + 1).join("\n"))
   }, [cursorIndex])
 
-  useEffect(() => {
-    if (reading) {
-      playAudio(chunks[cursorIndex], () => {
-        console.log("reading", reading, cursorIndex)
-        setCursorIndex((x) => x + 1)
-        // setReading(false)
-      })
-    }
-  }, [reading, cursorIndex])
+  // useEffect(() => {
+  //   if (reading) {
+  //     playAudio(chunks[cursorIndex], () => {
+  //       console.log("reading", reading, cursorIndex)
+  //       setCursorIndex((x) => x + 1)
+  //       // setReading(false)
+  //     })
+  //   }
+  // }, [reading, cursorIndex])
 
   return (
-    <div>
-      <div className="max-w-screen-lg mx-auto border-x p-4 space-y-4">
-        {chunks.map((x) => (
-          <p>{x}</p>
+    <div className="pt-12 pb-24 lg:py-24">
+      <article className="prose lg:prose-2xl mx-auto dark:prose-invert px-2">
+        {/* <div className="max-w-screen-lg mx-auto border-x p-4 space-y-4"> */}
+        <h1>Crazy New Ideas</h1>
+        <h5>May 2021</h5>
+        <div className="space-x-2">
+          <Button onClick={() => setSpeed(1)}>1x</Button>
+          <Button onClick={() => setSpeed(1.5)}>1.5x</Button>
+          <Button onClick={() => setSpeed(2)}>2x</Button>
+        </div>
+        <hr />
+        {chunks.map((x, id) => (
+          <p key={id}>{x}</p>
         ))}
-      </div>
+      </article>
       <div className="fixed bottom-4 right-4">
         <Button>
           {reading ? (
             <PauseCircleIcon
-              onClick={() => setReading(false)}
+              onClick={() => togglePlay()}
               className="w-12 h-12"
             />
           ) : (
             <PlayCircleIcon
-              onClick={() => setReading(true)}
+              onClick={() => togglePlay()}
               className="w-12 h-12"
             />
           )}
