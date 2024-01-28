63c77966dc274a836e439b84e5419fcd161d5f32
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2024-01-28 09:24:34 +0000

    docs(): init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import "./globals.css";
-import "./button.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
+import "./globals.css"
+import "./button.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { Toaster } from "react-hot-toast"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Voice Assistant",
-  description: "Siri, but for the web",
-};
+  title: "Audiflo",
+  description: "Chat to your audiobook",
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -23,5 +23,5 @@ export default function RootLayout({
         <Toaster />
       </body>
     </html>
-  );
+  )
 }
